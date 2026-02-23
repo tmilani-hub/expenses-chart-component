d10b986e52c485ea279b9aed9a0a4c50fc954797
tmilani-hub <tmilani@edenschool.fr>
2026-02-24 00:06:15 +0100

    msg

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // ALL queryselector
 const $inp_spend = document.querySelector("#depense");
 const $form = document.querySelector("#form");
-const $liste = document.querySelector("#liste");
 const $h2 = document.querySelector("#h2");
+const $liste = document.querySelector("#liste")
 // ALL variable
 let liste = [];
+let liste_object = [];
 // ALL funcion
 function CrateObjecListeDiv(word) {
 	const $div = document.createElement("div");
@@ -18,33 +19,32 @@ function CrateObjecListeDiv(word) {
 	$div.appendChild($btn);
 	$btn.addEventListener("click", () => {
 		$div.remove();
-		for (let i = 0; i < liste.length; i++) {
-			if ($p.textContent === liste[i]) {
-				liste[i].remove;
-				break;
-			}
-		}
+		for (let i = 0; i < liste.length; i++)
 	});
+	liste_object.appendChild($div)
 	return $div;
-}
-function ordonerListe() {
-	liste.appendChild($inp_spend.value);
-
-	if (liste.length < 1) {
-		$h2.classList.remove("none");
-	} else {
-		$h2.classList.add("none");
-	}
-	for (let i = 0; i < liste.length; i++) {
-		let object = CrateObjecListeDiv(liste[i]);
-		$liste.appendChild(object);
-	}
 }
 // ALL addeventlistener
 $form.addEventListener("submit", (e) => {
 	e.preventDefault();
 
-	ordonerListe();
+	for (let i = 0; i < liste.length; i++) [
+		liste_object[i].remove()
+	]
+	liste_object = []
+	liste.appendChild($inp_spend.value);
+	liste.sort();
+
+	for (let i = 0; i < liste.length; i++) {
+		let object = CrateObjecListeDiv(liste[i]);
+
+		$liste.appendChild(object);
+	}
+	if (liste.length < 1) {
+		$h2.classList.remove("none");
+	}else{
+		$h2.classList.add("none");
+	}
 
 	$form.reset();
 });
