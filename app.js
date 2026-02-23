ce27a87dcd297ff2ccd8c137971ffa02ac8a0ae7
tmilani-hub <tmilani@edenschool.fr>
2026-02-23 17:16:38 +0100

    msg

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
+// ALL queryselector
 const $inp_spend = document.querySelector("#depense");
-const $btn_for_spend = document.querySelector("#btnfordepense");
+const $form = document.querySelector("#form");
 const $liste = document.querySelector("#liste");
-
+const $h2 = document.querySelector("#h2");
+// ALL variable
+let liste = [];
+// ALL funcion
 function CrateObjecListeDiv(word) {
 	const $div = document.createElement("div");
 	const $p = document.createElement("p");
-
+	const $btn = document.createElement("button");
 	$div.classList.add("listeobject");
-
+	$btn.classList.add("btn-primary");
 	$p.textContent = word;
-
+	$btn.textContent = "suprimer";
 	$div.appendChild($p);
-
+	$div.appendChild($btn);
+	$btn.addEventListener("click", () => {
+		$div.remove();
+		for (let i = 0; i < liste.length; i++) {
+			if ($p.textContent === liste[i]) {
+				liste[i].remove;
+				break;
+			}
+		}
+	});
 	return $div;
 }
+function ordonerListe() {
+	liste.appendChild($inp_spend.value);
 
-$btn_for_spend.addEventListener("click", () => {
-	if ($inp_spend.value === "") {
-		console.log("error");
+	if (liste.length < 1) {
+		$h2.classList.remove("none");
 	} else {
-		let div = CrateObjecListeDiv($inp_spend.value);
-		$liste.appendChild(div);
+		$h2.classList.add("none");
 	}
+	for (let i = 0; i < liste.length; i++) {
+		let object = CrateObjecListeDiv(liste[i]);
+		$liste.appendChild(object);
+	}
+}
+// ALL addeventlistener
+$form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	ordonerListe();
+
+	$form.reset();
 });
